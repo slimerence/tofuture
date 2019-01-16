adcd56e9e9e6f7e89098d7073204a65777024298
slimerence <39113678+slimerence@users.noreply.github.com>
2019-01-16 15:27:33 +1100

    up

--- tofuture/pages/kuaiji.blade.php
+++ tofuture/pages/kuaiji.blade.php
@@ -7,10 +7,63 @@
             </div>
         </div>
     </section>
-    <section>
-        <div class="container" style="padding: 0 10%;">
-            <img src="{{ asset('images/tofuture/k1.jpg') }}" alt="kuaiji" class="img-fluid w-100">
-            <img src="{{ asset('images/tofuture/k2.jpg') }}" alt="kuaiji" class="img-fluid w-100">
+    <section class="ptb-60">
+        <div class="container">
+            <div class="about-item jinrong">
+                <h2 class="text-center">ToFuture本地会计就业集训营 课表</h2>
+                <div class="whr"></div>
+                <div class="box">
+                    <h3><i class="fa fa-caret-right" aria-hidden="true"></i>第一天 深度分析澳洲会计行业，精细制定个人职业规划</h3>
+                    <ul>
+                        <li>澳洲会计行业结构初解析：趋势，竞争和机会</li>
+                        <li>剖析澳洲最热会计岗位和相关职位</li>
+                        <li>根据HR招聘内部资料进行主要岗位职责和要求深度分析</li>
+                        <li>为工作经验有限的毕业生传授寻找工作的有效战略</li>
+                        <li>会计实操1: Bookkeeping A/R A/P工作内容+软件教学</li>
+                    </ul>
+                </div>
+                <hr class="mtb-15">
+
+                <div class="box">
+                    <h3><i class="fa fa-caret-right" aria-hidden="true"></i>第二天 会计行业进入指南及脱颖而出的技能培训</h3>
+                    <ul>
+                        <li>雇主想要员工具备的技能</li>
+                        <li>应聘工作该做的准备和技巧</li>
+                        <li>会计实操2: Payroll BAS Financial Report工作内容+软件教学</li>
+                        <li>会计实操3: Month End Job Duties, Fix assets and other duties工作内容讲解</li>
+                    </ul>
+                </div>
+                <hr class="mtb-15">
+
+                <div class="box">
+                    <h3><i class="fa fa-caret-right" aria-hidden="true"></i>第三天 专业会计简历以及cover letter打造宝典</h3>
+                    <ul>
+                        <li>根据每位同学自身特点打造专业的会计简历</li>
+                        <li>剖析专业简历应该满足的要求</li>
+                        <li>深度讲解一份成功简历的要素，让你满足雇主的要求</li>
+                        <li>应用在简历中除会计岗位的工作经验</li>
+                        <li>专业的Cover Letter设计和技巧，改良和整合关键字</li>
+                        <li>Cover Letter详解及传授避免雷区的技巧，针对目标职位进行实战训练</li>
+                        <li>升职求职软实力1: 澳洲大中小型公司招聘流程特点及讲解</li>
+                    </ul>
+                </div>
+                <hr class="mtb-15">
+
+                <div class="box">
+                    <h3><i class="fa fa-caret-right" aria-hidden="true"></i>第四天 传授面试终极技能，精讲不可不避之面试雷区</h3>
+                    <ul>
+                        <li>面试和电话面试介绍及分析</li>
+                        <li>面试中“一定要做”和“一定不要做”的注意事项</li>
+                        <li>会计从业人员着装要求</li>
+                        <li>面试问题和回答分析</li>
+                        <li>面试打分标准解析</li>
+                        <li>实战演练，模拟面试及有效反馈</li>
+                        <li>职求职软实力2: 面试技巧及行为测试讲解</li>
+                    </ul>
+                </div>
+
+            </div>
+
         </div>
     </section>
 
