9b1bb9d980b9312515dbd33d5791fc2c2507e28c
slimerence <39113678+slimerence@users.noreply.github.com>
2019-01-17 11:07:46 +1100

    u

---
+++ tofuture/pages/baoguo.blade.php
@@ -0,0 +1,133 @@
+@extends(_get_frontend_layout_path('frontend'))
+@section('content')
+    <section class="inner-intro dark-bg bg-image overlay-dark parallax parallax-background1" data-background-img="{{ url('images/tofuture/full/absbanner.jpg') }}">
+        <div class="container">
+            <div class="row title">
+                <h2 class="h2">CFA保过</h2>
+            </div>
+        </div>
+    </section>
+    <!-- End Intro Section -->
+    <section class="pt-80">
+        <div class="container">
+            <div class="about-item">
+                <h2 class="text-center">WinCareer CFA</h2>
+                <div class="whr"></div>
+                <p>WinCareer是澳洲未来教育集团旗下专注于职业培训的子品牌，未来教育作为墨尔本第一老牌商科辅导培训机构，累计服务超过50000名留学生，而WinCareer CFA正是整合了未来教育最强金融师资团队，根据各大高校商科课程以及澳洲考生特别编写了独家教材，结合最新考纲整理详细讲义，设置最科学的课程结构，推出了全澳最专业最负责的CFA一级保过课程</p>
+
+                <hr class="mtb-15">
+                <div class="row">
+                    <div class="col-12">
+                        <h3><i class="fa fa-caret-right" aria-hidden="true"></i>课程优势</h3>
+                        <ul>
+                            <li>科学的课程设置：我们结合澳洲考生特点设置了三大学习阶段，知识精讲班，冲刺串讲刷题班，全真模拟班。</li>
+                            <li>专业的导师团队</li>
+                            <li>独家编写的教材：我们根据澳洲各大高校商科课程特点撰写教材，详细阐释CFA所有考点</li>
+                            <li>丰富的教学资源：我们提供线下课堂教学，线上课程视频，同学可以随时观看，以及CFA自习室帮助大家巩固所学知识</li>
+                            <li>两次全真模拟考试：考试高度还原真实考场，更有12年全真题目分享，多次测评找出自身的薄弱的环节，体验考试过程便于考生调整至最佳考试状态</li>
+                            <li>双重保过承诺：报名CFA课程的学员将签订贴心的保过协议，如果没有通过6月的考试，我们将全额支付你12月的考试费用，并赠送新的CFA课程的旁听资格</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="pt-15 pb-5  bg-lt">
+        <div class="container">
+            <div class="about-item">
+                <h2 class="text-center">课程安排</h2>
+                <div class="whr"></div>
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-6">
+                        <h3><i class="fa fa-caret-right" aria-hidden="true"></i>知识精讲</h3>
+                        <ul>
+                            <li>16天96个小时习题精讲＋16个小时应用练习</li>
+                            <li>精讲CFA一级所有知识点</li>
+                            <li>旨在让同学们掌握所有reading框架</li>
+                            <li>及所有CFA考试细点与难点</li>
+                        </ul>
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-6">
+                        <h3><i class="fa fa-caret-right" aria-hidden="true"></i>冲刺串讲刷题班</h3>
+                        <ul>
+                            <li>5天30个小时串讲＋10个小时冲刺刷题</li>
+                            <li>凝练CFA所有考点并大量刷题巩固reading框架</li>
+                            <li>了解CFA考试难度加强做题技巧与时间把控能力</li>
+                            <li>加快做题速度提高正确率</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr class="mtb-15">
+                <div class="row">
+                    <div class="col-12 col-md-6 col-lg-6">
+                        <h3><i class="fa fa-caret-right" aria-hidden="true"></i>全真模拟班</h3>
+                        <ul>
+                            <li>多次全真模拟</li>
+                            <li>考试高度还原真实考场</li>
+                            <li>12年全真题目分享</li>
+                            <li>多次测评找出自身的薄弱的环节</li>
+                            <li>体验考试过程便于考生调整至最佳考试状态</li>
+                        </ul>
+
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-6">
+                        <h3><i class="fa fa-caret-right" aria-hidden="true"></i>双重保过</h3>
+                        <p>保过协议</p>
+                        <ul>
+                            <li>缺勤小于三次</li>
+                            <li>通过一次模拟考试</li>
+                        </ul>
+                        <p>若满足以上条件：</p>
+                        <ul>
+                            <li>赠送下一期WinCareerCFA课程</li>
+                            <li>免除学生下一次的考试报名费用</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+
+    <section class="pt-80 pb-5">
+        <div class="container">
+            <div class="about-item">
+                <h2 class="text-center">Wincareer最强师资团队</h2>
+                <div class="whr"></div>
+                <div class="row">
+                    <div class="col-lg-6 col-sm-12">
+                        <h3><i class="fa fa-caret-right" aria-hidden="true"></i>金牌导师Jasmine</h3>
+                        <div class="row">
+                            <div class="col">
+                                <img class="img-fluid" style="max-width: 250px" src="{{ asset('images/tofuture/team/js.jpg') }}" alt="" />
+                            </div>
+                            <div class="col">
+                                <p>多年CFA从教经验，在Monash作为tutor留校教学，了解各大高校商科课程，有的放矢地选择性教学，在WinCareer CFA教学保持学员满分好评。</p>
+                                <p><strong>擅长科目：</strong>FRA，Economics, Quantitative</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-lg-6 col-sm-12">
+                        <h3><i class="fa fa-caret-right" aria-hidden="true"></i>金牌导师Tony</h3>
+                        <div class="row">
+                            <div class="col">
+                                <img class="img-fluid" style="max-width: 250px" src="{{ asset('images/tofuture/team/tony.jpg') }}" alt="" />
+                            </div>
+                            <div class="col">
+                                <p>12年墨尔本各大名校金融专业授课经验，逻辑清晰，风趣幽默，思维导图教学法，将难题一网打尽。</p>
+                                <p><strong>擅长科目：</strong>Corporate Finance, Fixed Income, Equity Investment, Derivatives, Portfolio Management</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+@endsection
