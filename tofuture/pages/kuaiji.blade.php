2979442d48e69b74496c7228ba28db15dc5f9357
JennyWang <zhewang0414@gmail.com>
2019-05-31 11:51:51 +1000

    内容修改

--- tofuture/pages/kuaiji.blade.php
+++ tofuture/pages/kuaiji.blade.php
@@ -3,7 +3,8 @@
     <section class="inner-intro dark-bg bg-image overlay-dark parallax parallax-background1" data-background-img="{{ url('images/tofuture/full/absbanner.jpg') }}">
         <div class="container">
             <div class="row title">
-                <h1 class="h2">会计训练营 | CPA 证书培训 | 注册会计师</h1>
+                <h1 class="h2">会计训练营 CPA 证书培训</h1>
+                <h2 class="h3">CPA 证书培训 | 注册会计师</h2>
             </div>
         </div>
     </section>
@@ -12,7 +13,7 @@
             <div class="about-item jinrong">
                 <h2 class="text-center">ToFuture本地会计就业集训营 课表</h2>
                 <div class="whr"></div>
-                <p>WinCareer 提供CPA证书培训，帮助想要获得注册会计师资格的同学们通过考试。Tofuture的本地会计训练营提供全面的培训服务，帮助同学们进入会计职场。</p>
+                <p>WinCareer 提供CPA证书培训，帮助想要获得注册会计师资格的同学们通过考试。Tofuture的本地会计训练营提供全面的培训服务，帮助同学们进入会计职场。拥有CPA证书意味着你有了多方面就业发展的机会，并且有利于在会计行业的职业发展。注册会计师作为可以从事社会审计及其他审计工作的专业人士，薪资也非常丰厚。</p>
                 <div class="box">
                     <h3><i class="fa fa-caret-right" aria-hidden="true"></i>第一天 深度分析澳洲会计行业，精细制定个人职业规划</h3>
                     <ul>
@@ -29,7 +30,7 @@
                     <h3><i class="fa fa-caret-right" aria-hidden="true"></i>第二天 会计行业进入指南及脱颖而出的技能培训</h3>
                     <ul>
                         <li>雇主想要员工具备的技能</li>
-                        <li>应聘工作该做的准备和技巧</li>
+                        <li>应聘工作该做的准备和技巧，cpa证书的重要性</li>
                         <li>会计实操2: Payroll BAS Financial Report工作内容+软件教学</li>
                         <li>会计实操3: Month End Job Duties, Fix assets and other duties工作内容讲解</li>
                     </ul>
