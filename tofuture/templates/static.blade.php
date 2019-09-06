c142f9675005130e8a0228f438e85bead8ef025d
slimerence <39113678+slimerence@users.noreply.github.com>
2019-09-06 16:27:28 +1000

    u

--- tofuture/templates/static.blade.php
+++ tofuture/templates/static.blade.php
@@ -1,6 +1,6 @@
 @extends(_get_frontend_layout_path('frontend'))
 @section('content')
-    <section class="inner-intro dark-bg bg-image overlay-dark40 " data-background-img="{{ url('images/tofuture/full/absbanner8.jpg') }}" style="background-size: cover;background-position: center center">
+    <section class="inner-intro dark-bg bg-image overlay-dark " data-background-img="{{ url('images/tofuture/full/absbanner9.jpg') }}" style="background-size: cover;background-position: center center">
         <div class="container">
             <div class="row title">
                 <h1 class="h2" style="text-shadow: 2px 2px 4px #000c;">{{ $page->title }}</h1>
